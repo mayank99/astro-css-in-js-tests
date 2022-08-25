0ce556c25b5a0e305464af85011d1eeb45da6494
Mayank <mayank99@users.noreply.github.com>
2022-08-25 11:08:44 -0400

    Try to fix typestyle SSR

--- typestyle/src/components/Styles.tsx
+++ typestyle/src/components/Styles.tsx
@@ -1,5 +1,18 @@
-import { getStyles } from 'typestyle';
+import * as React from 'react';
+import { getStyles, setStylesTarget } from 'typestyle';
 
-export default () => (
-	<style id='typestyle' dangerouslySetInnerHTML={{ __html: getStyles() }} />
-);
+export default () => {
+	const ref = React.useRef<HTMLStyleElement>();
+
+	React.useEffect(() => {
+		setStylesTarget(ref.current);
+	}, []);
+
+	return (
+		<style
+			id='typestyle'
+			ref={ref}
+			dangerouslySetInnerHTML={{ __html: getStyles() }}
+		/>
+	);
+};
