ebabf20396b01f03c3a6e8e7c33c83c7b40ce19c
Mayank <mayank99@users.noreply.github.com>
2022-09-08 11:25:56 -0400

    Update linaria

--- linaria/astro.config.ts
+++ linaria/astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import linaria from '@linaria/rollup/esm';
+import linaria from '@linaria/rollup';
 
 // https://astro.build/config
 export default defineConfig({
