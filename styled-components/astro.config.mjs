818d3742df8980798c417ff3a1a65f7ea4f5a9d3
Mayank <9084735+mayank99@users.noreply.github.com>
2022-12-29 15:48:32 +0000

    fix styled-components example

--- styled-components/astro.config.mjs
+++ styled-components/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
 	vite: {
-		legacy: { buildSsrCjsExternalHeuristics: import.meta.env.SSR },
+		legacy: { buildSsrCjsExternalHeuristics: !isDev },
 		ssr: { noExternal: ['styled-components'] },
 	},
 });
