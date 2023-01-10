ec231f708eb5f7721827a2eedbb0a2e674053925
Mayank <mayank99@users.noreply.github.com>
2023-01-09 21:17:10 -0500

    try linaria vite plugin

--- linaria/astro.config.ts
+++ linaria/astro.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import linaria from '@linaria/rollup';
+import linaria from '@linaria/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		format: 'file',
+	},
 	vite: {
-		plugins: [linaria()],
-		legacy: { buildSsrCjsExternalHeuristics: true },
-		ssr: { noExternal: ['@linaria/*'] },
+		plugins: [
+			linaria({
+				displayName: true,
+				classNameSlug: (hash, title, args) => `${args.dir}_${title}_${hash}`,
+				babelOptions: {
+					presets: ['react'],
+				},
+			}),
+		],
+		css: {
+			modules: false,
+		},
 	},
 	integrations: [react()],
 });
